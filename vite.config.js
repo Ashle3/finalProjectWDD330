2e7ff900307e6112de25d4d30b75b6efba245863
Ashle3 <ashleeb3@byui.edu>
2023-04-03 13:21:15 -0500

    teacher and starting changes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "finalProjectWDD330/",
+  root: "src/",
 
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "finalProjectWDD330/views/index.html")
+        main: resolve(__dirname, "src/index.html")
       },
     },
   }
