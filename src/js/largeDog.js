d9cd32c884a523f08af5e4db44020709daaffb43
Ashle3 <ashleeb3@byui.edu>
2023-04-04 17:15:00 -0500

    i need help

---
+++ src/js/largeDog.js
@@ -0,0 +1,13 @@
+import largeDogs from "../json/largeDogs.json"  assert { type: 'JSON' };
+let test = JSON.stringify(largeDogs);
+console.log(test);
+
+
+// let test = document.getElementById('largedogs');
+
+// let dog = "";
+// for(dog=0, dog<largeDogs.length; dog++;){
+//     // let test = document.getElementById('largedogs');
+//     test.innerHTML = largeDogs[dog].name;
+// }
+
