a59074990acb1dde8e3fb061a741eab8862cef68
Ashle3 <ashleeb3@byui.edu>
2023-04-03 22:14:57 -0500

    created new views

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,11 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        large: resolve(__dirname, "src/large-dog.html"),
+        small: resolve(__dirname, "src/small-dog.html"),
+        adoption: resolve(__dirname, "src/adoption-form.html"),
+        success: resolve(__dirname, "src/success.html")
       },
     },
   }
